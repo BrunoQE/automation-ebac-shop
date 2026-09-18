640e20f19879c25e8ffd9a4f5c27b64475995710
Bruno <brunoepucca@gmail.com>
2026-09-18 10:51:42 -0300

    aumento global da espera

--- playwright.config.js
+++ playwright.config.js
@@ -23,6 +23,7 @@ const testDir = defineBddConfig({
  */
 export default defineConfig({
   testDir,
+  timeout: 60_000,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
